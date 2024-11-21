d001ee4a88c62391a0d9a88e0a31252a12c9ddd3
Ofek Katzav <ofekkatzav@h-MacBook-Pro-sl-Ofek.local>
2024-11-21 22:32:39 +0200

    done7

--- CalcPi.java
+++ CalcPi.java
@@ -18,6 +18,7 @@ public class CalcPi {
 	piAppro*=4;
 	System.out.println("pi according to Java: " + Math.PI);
 	System.out.println("pi, approximated:    " + piAppro);
+	System.out.println();
 
 	}
 }
